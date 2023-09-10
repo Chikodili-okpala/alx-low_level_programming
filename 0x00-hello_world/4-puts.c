876aa7d9c9c973d0e36158d9bee5f080258786b8
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-10 14:51:42 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * This is the main function.
- * It prints a message using the puts function.
- *
- * return 0 upon successful execution.
- */
-
+ * main - This is the main function
+ * puts - Prints Programming is like building a multilingual puzzle
+ * @ return (0) - Returns o upon execution
+  */ 
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
