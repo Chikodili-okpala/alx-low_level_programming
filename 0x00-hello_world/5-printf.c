e8148b5de774069a83f71db0b3a7c9b4abb69431
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-10 15:06:11 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function.
+ * It prints a message using the puts function.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
