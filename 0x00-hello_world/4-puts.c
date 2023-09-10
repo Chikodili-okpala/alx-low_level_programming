9e07684a5870497ecabaf961af2b47ec0336e4c7
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-10 14:21:08 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void) /*Print what programming is like*/
+int main (void) /*Print what programming is like*/
 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
