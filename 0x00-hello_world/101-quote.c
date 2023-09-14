839be1927ed39dccc50969dce0ca02033dbc713d
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-14 11:54:31 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+/**
+ * main - This is the main function.
+ * It prints a message using the fprintf function.
+ *
+ * Return: 1 (indicating successful execution)
+ */
+
+{
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return (1);
+}
