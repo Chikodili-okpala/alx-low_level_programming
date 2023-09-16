cf36dd45fbac462da028e8f86676b3c7d002951c
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-16 13:45:36 -0700

    exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the sizes of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	/*exclude 'e' and 'q'*/
+		if (letter != 'e' && letter != 'q')
+		
+		{
+	/*prints the letters*/
+			putchar(letter);
+		}
+	
+	}
+
+	/*prints nw line aftr each letter*/
+	putchar('\n');
+
+	return (0);
+}
