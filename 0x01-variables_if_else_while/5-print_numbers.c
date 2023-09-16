e4573df68a9f8ddd7af39eac44620c79c5839938
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-16 14:12:10 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the sizes of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	int n;
+
+	/*initialize n to 0*/
+	for (n = 0; n <= 9; n++)
+	{
+	/*prints the digits as character*/
+		putchar(n + '0');
+	}
+	
+	/*prints new line after the single digits*/
+	putchar('\n');
+
+	return (0);
+
+}
