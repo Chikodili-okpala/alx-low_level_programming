541d99e25bd20e853771a5803e1c8b49a8a601b5
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-16 12:55:00 -0700

    lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,13 +12,13 @@ int main(void)
 	char letter;
 
 	/*prints lowercase letters*/
-	for( letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
 
 	/*prints uppercase letters*/
-	for( letter = 'A'; letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
