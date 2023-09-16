a4db46eb6abab17824761b76196672caa3e2fdca
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-16 01:23:08 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,11 @@ int main(void)
 	{
 	/*if n is greater than zero print*/
 		printf("%d is positive\n", n);
-	}
-	else if  (n == 0) 
+	} else if  (n == 0) 
 	{
 	/* if n is equal to zero print*/
 		printf("%d is zero\n", n);
-	}
-       	else if (n < 0) 
+	} else if (n < 0) 
 	{
 	/* if n is less than zero print*/
 		printf("%d is negative\n", n);
