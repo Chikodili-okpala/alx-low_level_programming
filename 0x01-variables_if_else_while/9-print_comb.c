35708cae5d71dbb89bfe9273b2cecc4d6c0f3377
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-16 22:27:42 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the sizes of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 9; m++)
+	{
+	for (n = 0; n <= 9; n++)
+	{
+	/*prints the combination*/
+		putchar(m + '0');
+		putchar(n + '0');
+
+	if (m != 9 || n != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
