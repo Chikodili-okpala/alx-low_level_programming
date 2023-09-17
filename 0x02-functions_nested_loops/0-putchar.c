144dd2f5744eeb2d04f9bec8a0188d30e8631ac0
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-17 03:56:33 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the sizes of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	/*prints the value and a new line*/
+	puts("_putchar");
+
+	return (0);
+}
