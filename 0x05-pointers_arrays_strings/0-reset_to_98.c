e70ded332fadba57f8ed85771f9f5cf2ae14be9b
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-18 19:38:39 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,23 +9,3 @@ void reset_to_98(int *n)
 	*n = 98;
 }
 
-/**
-*main - check the code
-*
-*Return: Always 0 (indicating successful execution)
-*/
-int main(void)
-{
-	int m = 70;
-	int *n = &m;
-
-		printf("Value of m before the call is %d\n", m);
-
-	reset_to_98(n);
-
-		printf("Value of m after the call is %d\n", m);
-
-	return (0);
-	
-}
-
