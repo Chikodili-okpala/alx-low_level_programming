88100897e328b9d9729abe02fb03366e0e7f21a4
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-18 20:34:44 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*void_puts - returns the length of a string
+*@str: a pointer to the string
+*
+* Description: This function takes a pointer to a string - character array,
+* as it's parameter and prints it to the standard output (stdout), followed
+* by a newline.
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	putchar(*str);
+	str++;
+	}
+
+	putchar('\n');
+}
+
