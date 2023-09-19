02118ed06b0249c0698334a9d3dae7b0baf49ea9
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-18 19:13:13 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*void reset_to_98 - set the integer pointed to 98
+*@n: a pointer to the integer we want to set to 98
+*/
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
+/**
+*main - check the code
+*
+*Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+	int m;
+	int *n;
+
+	m = 70;
+	*n = &m;
+
+		printf("Value of m before the call is %d\n", m);
+
+	reset_to_98(n);
+
+		printf("Value of m after the call is %d\n", m);
+
+	return (0);
+	
+}
+
