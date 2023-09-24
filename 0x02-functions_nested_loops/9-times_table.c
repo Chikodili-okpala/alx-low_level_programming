b861e1cda071c6939720581ec6d5e92fb016d99a
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-24 03:37:36 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return - returns 0 on succesful executiojn
+ */
+void times_table(void)
+{
+	int m = 0;
+	int n;
+	int INFINITY;
+
+	while (m == 9)
+	{
+		for (n = 0; n <= INFINITY; n++)
+		{
+			_putchar('m * n');
+			_putchar(',');
+			_putchar('\t');
+		}
+	}
+
+}
