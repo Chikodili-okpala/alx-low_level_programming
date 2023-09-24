ec3702298a80ccf32f300ee660d073941949b3c7
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-24 02:27:42 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - checks the absolute value of an integer
+ * @p: integer to be checked
+ *
+ * Return: returns the absolute value
+ */
+int _abs(int p)
+{
+	if (p < 0)
+	{
+		return (-p);
+	}
+	else
+	{
+	return (p);
+	}
+}
+
