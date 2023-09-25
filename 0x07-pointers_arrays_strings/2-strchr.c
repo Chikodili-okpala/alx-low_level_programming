401f7a514b9108f6ef2f763de3c2043efb3d101a
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-25 16:06:44 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - returns pointer to the 1st occurence of the character c
+ * in the string s
+ * @s: string value
+ * @c: character occurence
+ *
+ * Return:  a pointer to c
+ */
+char *_strchr(char *s, char c)
+{
+	int p;
+
+	/*checks for NULL pointer*/
+	if (s == NULL)
+	{
+	return (NULL);
+	}
+
+	/*loops to check for c char in the string s*/
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		if (s[p] == c)
+		{
+		/**
+		 * returns a pointer to the first occurrence
+		 * of the character c in the string s
+		 */
+		return (&(s[p]));
+		}
+	}
+
+		/*if c is not found, return NULL*/
+		return (NULL);
+
+}
+
