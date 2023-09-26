d5b1ee441a4675a52c421cff84bac9e337a0c1b7
Chikodili-okpala <amarachikodili@gmail.com>
2023-09-25 22:45:03 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets pointer to a pointer
+ * @s: pointer to a pointer
+ * @to: pointer to a char var
+ *
+ * Return:  0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
